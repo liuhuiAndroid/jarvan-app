37aa22051f1b3a0923a86174af6705dd0852bae4
lh <im.lh@hotmail.com>
2020-07-08 14:38:12 +0800

    Test

--- app/src/main/java/com/jarvan/app/JarvanApplication.kt
+++ app/src/main/java/com/jarvan/app/JarvanApplication.kt
@@ -1,25 +1,20 @@
 package com.jarvan.app
 
-import android.R
 import android.app.Application
 import android.content.Context
-import android.os.Environment
 import com.alibaba.android.arouter.launcher.ARouter
-import com.jarvan.app.utilities.DynamicTimeFormat
 import com.scwang.smartrefresh.layout.SmartRefreshLayout
 import com.scwang.smartrefresh.layout.footer.ClassicsFooter
 import com.scwang.smartrefresh.layout.header.ClassicsHeader
-import okio.Okio
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.androidXModule
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.singleton
 import timber.log.Timber
-import java.io.File
 
 /**
- * ajin233333
+ * lxy1960827933
  */
 class JarvanApplication : Application(), KodeinAware {
 
